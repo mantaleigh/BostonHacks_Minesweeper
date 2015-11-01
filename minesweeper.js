3b346550a3b252cbddc77d99be4d7099f5bb7f55
Samantha Voigt <samantha@Samanthas-MacBook-Pro.local>
2015-10-31 23:29:51 -0400

    re-organized into cell objects with images

--- minesweeper.js
+++ minesweeper.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     var board = [];
     for(var i = 0; i < c; i++) { 
       board[i] = [];
-      for (var j = 0; j < r; j++) board[i][j] = 0;
+      for (var j = 0; j < r; j++) board[i][j] = new Cell(i, j);
     } 
 
     // Choose mine locations randomly
@@ -18,31 +18,32 @@ $(document).ready(function() {
     while (minesPlaced < numMines){ 
       var x = Math.floor(Math.random()*c); 
       var y = Math.floor(Math.random()*r);
-      if (board[x][y] != 'X') { 
-        board[x][y] = 'X';
+      if (!board[x][y].isMine) { 
+        board[x][y].isMine = true;
         minePositions.push([x,y]);
         if (x > c-1) {
-          board[x-1][y]++;
+          board[x-1][y].content++;
           if (y > r-1) { 
-            board[x-1][y-1]++;
+            board[x-1][y-1].content++;
           }
-          if (y < 0) board[x-1][y+1]++;
+          if (y < 0) board[x-1][y+1].content++;
         }
-        if (y < r-1) board[x][y+1]++;
-        if (y > 0) board[x][y-1]++;
+        if (y < r-1) board[x][y+1].content++;
+        if (y > 0) board[x][y-1].content++;
         if (x < c-1) { 
-          board[x+1][y]++;
-          if (y < r-1) board[x+1][y+1]++;
-          if (y > 0) board[x+1][y-1]++;
+          board[x+1][y].content++;
+          if (y < r-1) board[x+1][y+1].content++;
+          if (y > 0) board[x+1][y-1].content++;
         }
         minesPlaced++;
+        console.log('placed mine number: ' + minesPlaced);
       }
     }
 
     console.log(minePositions);
     // Make sure mines are placed correctly
     for (var i = 0; i < minePositions.length; i++) { 
-      board[minePositions[i][0]][minePositions[i][1]] = 'X';
+      board[minePositions[i][0]][minePositions[i][1]].isMine = true;
     }
     console.log(board);
     return board;
@@ -59,10 +60,21 @@ $(document).ready(function() {
           row.className = "row";
           for (var j = 0; j < boardData[i].length; j++) {
              var cell = row.insertCell(j);
-             cell.innerHTML = boardData[i][j];
+             var img = document.createElement('img');
+             img.src = boardData[i][j].imgsrc;
+             cell.appendChild(img);
              cell.className = "cell";
           }
         }
       }
 
-})
+    // cell object
+    function Cell(x, y) { 
+      this.x = x;
+      this.y = y;
+      this.imgsrc = 'img/unclicked.png';
+      this.content = 0; 
+      this.isMine = false; 
+    } 
+
+});
