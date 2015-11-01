16a1c6ce6e72c3f8bf49435c9ac7ce9c68083646
UmutAkkaya <uba1995a@gmail.com>
2015-11-01 01:05:11 -0400

    added new stuff

--- minesweeper.js
+++ minesweeper.js
@@ -1,110 +1,86 @@
-<<<<<<< HEAD
 $(document).ready(function () {
-
-    var output = document.getElementById("output");
-    var b = createBoard(15, 15, 10);
-    addBoard(b);
-
-    function createBoard(r, c, numMines) {
-        var minePositions = [];
-        // the easy board is 9x9 and has 10 mines
-        var board = [];
-        for (var i = 0; i < c; i++) {
-            board[i] = [];
-            for (var j = 0; j < r; j++) board[i][j] = 0;
-        }
-        // not pretty
-        // select 10 random positions to put a mine and place it there (spot has X)
-        var minesPlaced = 0;
-        while (minesPlaced < numMines) {
-            var x = Math.floor(Math.random() * c);
-            var y = Math.floor(Math.random() * r);
-            if (board[x][y] != 'X') {
-                board[x][y] = 'X';
-                minePositions.push([x, y]);
-                if (x > c - 1) {
-                    board[x - 1][y]++;
-                    if (y > r - 1) {
-                        board[x - 1][y - 1]++;
-                    }
-                    if (y < 0) board[x - 1][y + 1]++;
-                }
-                if (y < r - 1) board[x][y + 1]++;
-                if (y > 0) board[x][y - 1]++;
-                if (x < c - 1) {
-                    board[x + 1][y]++;
-                    if (y < r - 1) board[x + 1][y + 1]++;
-                    if (y > 0) board[x + 1][y - 1]++;
-                }
-                minesPlaced++;
-            }
-=======
-$(document).ready(function() { 
 
     var b = createBoard(9, 9, 10);
     addBoard(b);
 
     function createBoard(r, c, numMines) {
 
-    // Create board size depending on input
-    var minePositions = [];
-    var board = [];
-    for(var i = 0; i < c; i++) { 
-      board[i] = [];
-      for (var j = 0; j < r; j++) board[i][j] = new Cell(i, j);
-    } 
-
-    // Choose mine locations randomly
-    var minesPlaced = 0;
-    while (minesPlaced < numMines){ 
-      var x = Math.floor(Math.random()*c); 
-      var y = Math.floor(Math.random()*r);
-      if (!board[x][y].isMine) { 
-        board[x][y].isMine = true;
-        minePositions.push([x,y]);
-        if (x > c-1) {
-          board[x-1][y].content++;
-          if (y > r-1) { 
-            board[x-1][y-1].content++;
-          }
-          if (y < 0) board[x-1][y+1].content++;
+        // Create board size depending on input
+        var minePositions = [];
+        var board = [];
+        for (var i = 0; i < c; i++) {
+            board[i] = [];
+            for (var j = 0; j < r; j++) board[i][j] = new Cell(i, j);
         }
-        if (y < r-1) board[x][y+1].content++;
-        if (y > 0) board[x][y-1].content++;
-        if (x < c-1) { 
-          board[x+1][y].content++;
-          if (y < r-1) board[x+1][y+1].content++;
-          if (y > 0) board[x+1][y-1].content++;
->>>>>>> cc4b9ac9f165ea07eab9834a5f95354d1cdbe1d6
+
+        // Choose mine locations randomly
+        var minesPlaced = 0;
+        while (minesPlaced < numMines) {
+            var x = Math.floor(Math.random() * c);
+            var y = Math.floor(Math.random() * r);
+            if (!board[x][y].isMine) {
+                board[x][y].isMine = true;
+                minePositions.push([x, y]);
+                if (x > c - 1) {
+                    board[x - 1][y].content++;
+                    if (y > r - 1) {
+                        board[x - 1][y - 1].content++;
+                    }
+                    if (y < 0) board[x - 1][y + 1].content++;
+                }
+                if (y < r - 1) board[x][y + 1].content++;
+                if (y > 0) board[x][y - 1].content++;
+                if (x < c - 1) {
+                    board[x + 1][y].content++;
+                    if (y < r - 1) board[x + 1][y + 1].content++;
+                    if (y > 0) board[x + 1][y - 1].content++;
+                }
+                minesPlaced++;
+            }
         }
 
         console.log(minePositions);
-
+        // Make sure mines are placed correctly
         for (var i = 0; i < minePositions.length; i++) {
-            board[minePositions[i][0]][minePositions[i][1]] = 'X';
+            board[minePositions[i][0]][minePositions[i][1]].isMine = true;
         }
         console.log(board);
         return board;
     }
 
-<<<<<<< HEAD
-    // working on this
-
+    // Method that dynamically creates a board depending on size.
     function addBoard(boardData) {
-        var mainDiv = $('#board');
-        var result = "<table border=1>";
+        // Fetch table reference  
+        var table = document.getElementById("board");
+        table.className = "table";
+        // Add rows and cells
         for (var i = 0; i < boardData.length; i++) {
-            result += "<tr>";
+            var row = table.insertRow(i);
+            row.className = "row";
             for (var j = 0; j < boardData[i].length; j++) {
-                result += "<td>" + boardData[i][j] + "</td>";
+                var cell = row.insertCell(j);
+                var img = document.createElement('img');
+                img.src = boardData[i][j].imgsrc;
+                cell.appendChild(img);
+                cell.className = "cell";
             }
-            result = result + "</tr>";
         }
-        mainDiv.append(result);
     }
+
+    // cell object
+    function Cell(x, y) {
+        this.x = x;
+        this.y = y;
+        this.imgsrc = 'img/unclicked.png';
+        this.content = 0;
+        this.isMine = false;
+        this.clicked = false;
+    }
+
 });
 
-
+//--------------------
+/*
 
 function concatData(id, data) {
     return id + ": " + data + "<br>";
@@ -203,44 +179,4 @@ function check_finger(pos) {
         }
     }
 }
-=======
-    console.log(minePositions);
-    // Make sure mines are placed correctly
-    for (var i = 0; i < minePositions.length; i++) { 
-      board[minePositions[i][0]][minePositions[i][1]].isMine = true;
-    }
-    console.log(board);
-    return board;
-  }
-
-    // Method that dynamically creates a board depending on size.
-    function addBoard(boardData) {
-        // Fetch table reference  
-        var table = document.getElementById("board");
-        table.className = "table";
-        // Add rows and cells
-        for (var i = 0; i < boardData.length; i++) {
-          var row = table.insertRow(i);
-          row.className = "row";
-          for (var j = 0; j < boardData[i].length; j++) {
-             var cell = row.insertCell(j);
-             var img = document.createElement('img');
-             img.src = boardData[i][j].imgsrc;
-             img.id = i + '-' + j;
-             cell.appendChild(img);
-             cell.className = "cell";
-          }
-        }
-      }
-
-    // cell object
-    function Cell(x, y) { 
-      this.x = x;
-      this.y = y;
-      this.imgsrc = 'img/unclicked.png';
-      this.content = 0; 
-      this.isMine = false; 
-    } 
-
-});
->>>>>>> cc4b9ac9f165ea07eab9834a5f95354d1cdbe1d6
+*/
